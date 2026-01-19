bc5c128e3a99b90a040ac1b0b5af9ed5f70db9ec
Diego <2863238@alu.murciaeduca.es>
2026-01-19 09:20:40 +0100

    Añadiendo el controlador de recursos a ciclos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,13 @@ namespace Database\Seeders;
 
 use App\Models\Proyecto;
 use App\Models\User;
+use Database\Seeders\CiclosTableSeeder;
 use Database\Seeders\FamiliasProfesionalesTableSeeder;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,9 +28,16 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]); */
 
+        Model::unguard();
+        Schema::disableForeignKeyConstraints();
+        
         self::seedProyectos();
         $this->command->info('Tabla catálogo inicializada con datos!');
         $this->call(FamiliasProfesionalesTableSeeder::class);
+        $this->call(CiclosTableSeeder::class);
+
+        Model::reguard();
+        Schema::enableForeignKeyConstraints();
     }
 
     private function seedProyectos():void
