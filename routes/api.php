d16ca791413ee085c2c77d9fb649cefbedc60065
Diego <2863238@alu.murciaeduca.es>
2026-02-03 16:34:43 +0100

    Configuracion AuthToken

--- routes/api.php
+++ routes/api.php
@@ -7,24 +7,32 @@ use Illuminate\Support\Facades\Route;
 use Psr\Http\Message\ServerRequestInterface;
 use Tqdev\PhpCrudApi\Api;
 use Tqdev\PhpCrudApi\Config\Config;
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\API\TokenController;
 
 // Rutas /api/v1
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('ciclos', CicloController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            $user = $request->user();
+            $user->fullName = $user->nombre . ' ' . $user->apellidos;
+            return $user;
+        });
 
+        Route::apiResource('ciclos', CicloController::class);
+    });
+    
     Route::apiResource('familias_profesionales', FamiliaProfesionalController::class)
-    ->parameters([
-        'familias_profesionales' => 'familiaProfesional'
-    ]);
+            ->parameters([
+                'familias_profesionales' => 'familiaProfesional'
+            ]);
+            
+    // emite un nuevo token
+    Route::post('tokens', [TokenController::class, 'store']);
+    // elimina el token del usuario autenticado
+    Route::delete('tokens', [TokenController::class, 'destroy'])->middleware('auth:sanctum');
 
 });
-
-
 
 // Rutas PHP-CRUD-API
 Route::any('/{any}', function (ServerRequestInterface $request) {
